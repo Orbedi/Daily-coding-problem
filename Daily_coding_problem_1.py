ba340a05e61242766bc84d5864c45ee34e312222
Orbedi <orlandobedi@outlook.es>
2018-07-08 00:03:42 +0100

    Coding problem 07/07/2018 - version 1 with tests

---
+++ Daily_coding_problem_1.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def add_two_num(list_, k):
+    for i in range(0, len(list_)):
+        for j in range(i+1, len(list_)):
+            if list_[i] + list_[j] == k:
+                return [list_[i], list_[j]]
+    return []
+
+
+class MyTest(unittest.TestCase):
+
+    def test_one_number(self):
+        self.assertEqual(add_two_num([1], 1), [])
+        self.assertEqual(add_two_num([1], 2), [])
+
+    def test_two_numbers(self):
+        self.assertEqual(add_two_num([1, 0], 1), [1, 0])
+        self.assertEqual(add_two_num([1, 0], 3), [])
+
+    def test_several_numbers(self):
+        self.assertEqual(add_two_num([1, 0, 2, 4, 6], 10), [4, 6])
+        self.assertEqual(add_two_num([1, 2, 4, 7], 10), [])
+
+    def test_empty(self):
+        self.assertEqual(add_two_num([], 0), [])
+        self.assertEqual(add_two_num([], 7), [])
+
+    def test_decimal_numbers(self):
+        self.assertEqual(add_two_num([1.5,3.6,2.5], 4), [1.5, 2.5])
+        self.assertEqual(add_two_num([1.5,3.6,2.5], 5.1), [1.5, 3.6])
+        self.assertEqual(add_two_num([1.5,3.6,2.5], 4.5), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
