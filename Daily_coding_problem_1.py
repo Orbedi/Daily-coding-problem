72cc3817c2266a2715ee8611ee7329a4cd77baaf
Orbedi <Orbedi@users.noreply.github.com>
2018-07-08 00:06:16 +0100

    Update Daily_coding_problem_1.py

--- Daily_coding_problem_1.py
+++ Daily_coding_problem_1.py
@@ -1,5 +1,9 @@
 import unittest
 
+""" Problem:
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+"""
 
 def add_two_num(list_, k):
     for i in range(0, len(list_)):
