a91cb6085961fc0e84203b5f72fcf4500d7df7a3
Orbedi <orlandobedi@outlook.es>
2018-07-11 01:11:12 +0100

    Daily coding problem - 08/07/2018

--- Daily_coding_problem_2.py
+++ Daily_coding_problem_2.py
@@ -1,3 +1,13 @@
+
+""" Problem:
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+"""
+
+
 def product_of_each_element(list_):
     result = [1] * len(list_)
     for i, elem_i in enumerate(list_):
@@ -17,15 +27,15 @@ def product_of_each_element_with_division(list_):
 
 
 def product_of_each_element_left_right(list_):
-    temp = 1
+    aux = 1
     result = [1] * len(list_)
     for i,elem in enumerate(list_):
-        result[i] = temp
-        temp *= elem
-    temp = 1
+        result[i] = aux
+        aux *= elem
+    aux = 1
     for i in range(len(list_)-1, -1, -1):
-        result[i] *= temp
-        temp *= list_[i]
+        result[i] *= aux
+        aux *= list_[i]
     return result
 
 
